364c52b74cd1d90067cac6f11ca84fa6f8b4f514
wei.zhou <554961776@qq.com>
2019-10-23 22:45:06 +0800

    斐波那契数列---生成器

--- demo/demo/samples/advance/do_generator.py
+++ demo/demo/samples/advance/do_generator.py
@@ -29,11 +29,23 @@ return RecUrsive(index - 1) + RecUrsive(index - 2);
 }
 '''
 print('def fib(max):斐波那契数列 https://blog.csdn.net/duanworld/article/details/68489796')
+'''
+https://www.liaoxuefeng.com/wiki/1016959663602400/1017318207388128
+注意，赋值语句：
+a, b = b, a + b
+相当于：
+t = (b, a + b) # t是一个tuple
+a = t[0]
+b = t[1]
+'''
 def fib(max):
     n, a, b = 0, 0, 1
+    print('1---n:{0},a:{1},b:{2},max:{3}'.format(n,a,b,max))
     while n < max:
         yield b
+        print('2-0---n:{0},a:{1},b:{2},max:{3}'.format(n, a, b, max))
         a, b = b, a + b
+        print('2-1---n:{0},a:{1},b:{2},max:{3}'.format(n, a, b, max))
         n = n + 1
     return 'done'
 
@@ -41,6 +53,7 @@ f = fib(10)
 print('fib(10):', f)
 for x in f:
     print(x)
+
 
 # call generator manually:
 g = fib(5)

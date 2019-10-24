5978cea065913c9faab3f32d29f582340bdcae65
wei.zhou <wei.zhou@macrowing.com>
2019-10-24 08:44:05 +0800

    日志

--- demo/demo/samples/debug/do_logging.py
+++ demo/demo/samples/debug/do_logging.py
@@ -2,9 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import logging
-logging.basicConfig(level=logging.INFO)
+from logging.handlers import RotatingFileHandler
+import os
 
-s = '0'
-n = int(s)
-logging.info('n = %d' % n)
-print(10 / n)
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+#定义一个RotatingFileHandler，最多备份3个日志文件，每个日志文件最大1K
+rHandler = RotatingFileHandler("log.txt",maxBytes = 1*1024,backupCount = 3)
+rHandler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+rHandler.setFormatter(formatter)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+
+logger.addHandler(rHandler)
+logger.addHandler(console)
+
+logger.info("Start print log")
+logger.debug("Do something")
+#os.path.
+logger.warning("Something maybe fail.")
+logger.info("Finish")
+
+

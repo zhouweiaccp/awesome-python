3f5b3e71a2b69492d4cdd3a75e7ac086023f4d2d
wei.zhou <554961776@qq.com>
2019-10-29 22:58:40 +0800

    logging logbook

---
+++ demo/demo/samples/commonlib/use_logbook_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# demo - 当前Project名称;
+# use_logbook_test - 在创建文件的对话框中指定的文件名;
+# chive - 当前用户名;
+# 2019/10/2922:49 - 当前系统日期;
+# 554961776@qq.com
+
+import os
+from use_logbook import run_log as logger
+
+if __name__ == '__main__':
+    logger.info("测试log模块，暂时就优化到这一步，后续再改进")
--- demo/demo/samples/context/do_with.py
+++ demo/demo/samples/context/do_with.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+with 语句实质是上下文管理。
+1、上下文管理协议。包含方法__enter__() 和 __exit__()，支持该协议对象要实现这两个方法。
+2、上下文管理器，定义执行with语句时要建立的运行时上下文，负责执行with语句块上下文中的进入与退出操作。
+3、进入上下文的时候执行__enter__方法，如果设置as var语句，var变量接受__enter__()方法返回值。
+4、如果运行时发生了异常，就退出上下文管理器。调用管理器__exit__方法。
+"""
+
+# 作者：skullfang
+# 链接：<a href='https://www.jianshu.com/p/5b01fb36fd4c'>https://www.jianshu.com/p/5b01fb36fd4c</a>
 
 from contextlib import contextmanager
 

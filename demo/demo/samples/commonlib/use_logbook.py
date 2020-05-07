0e62c9b0616b0f292ca5451fb7550c3dcd68052f
wei.zhou <554961776@qq.com>
2020-05-07 22:05:36 +0800

    日志 logbook  用户名

--- demo/demo/samples/commonlib/use_logbook.py
+++ demo/demo/samples/commonlib/use_logbook.py
@@ -9,14 +9,16 @@
 import os
 import sys
 import socket
+import getpass
 import logbook
 from logbook import Logger,StreamHandler,FileHandler,TimedRotatingFileHandler
 from logbook.more import ColorizedStderrHandler
 
 def log_type(record,handler):
-    log = "[{date}] [{hostname}] [{level}] [{filename}] [{func_name}] [{lineno}] {msg}".format(
+    log = "[{date}] [{hostname}] [{user}] [{level}] [{filename}] [{func_name}] [{lineno}] {msg}".format(
         date = record.time,                              # 日志时间
         hostname=socket.gethostname(),          # 主机名
+        user=getpass.getuser(),           # 用户名
         level = record.level_name,                       # 日志等级
         filename = os.path.split(record.filename)[-1],   # 文件名
         func_name = record.func_name,                    # 函数名
